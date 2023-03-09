68b032538cbc9f2622ecadb3024dfdc1329b9aa6
eqram1 <abmeqramhossain@gmail.com>
2023-03-10 00:23:07 +0600

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <button className="btn btn-outline btn-info">Info</button>
-<button className="btn btn-outline btn-success">Success</button>
-<button className="btn btn-outline btn-warning">Warning</button>
-<button className="btn btn-outline btn-error">Error</button>
+
     </div>
   );
 }
